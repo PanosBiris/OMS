8c9ca2d5047466aacc2dc4756930daf8fd4d0297
Panagiotis Biris <biris.panagiotis@gmail.com>
2025-03-18 22:13:45 +0200

    commit #6

--- Backend/PB.OMS.OrderingService/PB.OMS.OrderingService.Api/Program.cs
+++ Backend/PB.OMS.OrderingService/PB.OMS.OrderingService.Api/Program.cs
@@ -5,33 +5,62 @@ using PB.OMS.OrderingService.Infrastructure;
 using PB.OMS.OrderingService.Infrastructure.Repositories;
 using Scalar.AspNetCore;
 
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-builder.Services.AddApplication().AddInfrastructure();
-
-builder.Services.AddScoped<IOrderRepository, OrdersRepository>();
-var cs = builder.Configuration.GetConnectionString("OrdersDB");
-builder.Services.AddDbContext<OrdersContext>(opt => opt.UseSqlServer(cs));
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    app.MapOpenApi();
-    app.MapScalarApiReference();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        builder.Services.AddOpenApi();
+        builder.Services.AddApplication().AddInfrastructure();
+
+        builder.Services.AddScoped<IOrderRepository, OrdersRepository>();
+        var cs = builder.Configuration.GetConnectionString("OrdersDB");
+        builder.Services.AddDbContext<OrdersContext>(opt => opt.UseSqlServer(cs));
+
+        var app = builder.Build();
+
+        // Apply migrations automatically
+        ApplyMigrations(app);
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+            app.MapScalarApiReference();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+
+    private static void ApplyMigrations(WebApplication app)
+    {
+        using (var scope = app.Services.CreateScope())
+        {
+            var dbContext = scope.ServiceProvider.GetRequiredService<OrdersContext>();
+
+            // Check and apply pending migrations
+            var pendingMigrations = dbContext.Database.GetPendingMigrations();
+            if (pendingMigrations.Any())
+            {
+                Console.WriteLine("Applying pending migrations...");
+                dbContext.Database.Migrate();
+                Console.WriteLine("Migrations applied successfully.");
+            }
+            else
+            {
+                Console.WriteLine("No pending migrations found.");
+            }
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # OMS
 Order Management System Example
+
+This is a sample application of the ordering system.
+The application consists of 3 services (Ordering Service, Customer and Menu) each one created as a separate Rest Api using .NET Core 9 and a Single Page Application created using Angular 16.
+*Customer and Menu are not completed.
+
+The Ordering Service (PB.OMS.OrderingService sln) is created using the Clean Architecture Template and CQRS. Repository pattern and Dependency injection have been also used.
+It contains endpoints:
+- to create an order by selecing items from a menu
+- to retrieve a list of orders of a customer
+- to get the details of an order
+- In a separate controller there is an endpoint that can be used from restaurant staff to update the status of an order.
+
+Prerequisites to run the web api locally: VS 2022 + .NET 9.0, SQL Express or SQL Developer (connection string contains windows auth / server name: localhost)
+For the database / data layer it uses EF Core code first approach. Database and migrations should be created on startup.
+
+The UI / SPA is located in frontend folder in the repo.
+Prerequisites: node.js
+To run the angular app locally navigate to the root folder and run the following commands:
+- npm install
+- npm -i -g @angular/cli
+- ng serve
+The last one should make the SPA available in: http://localhost:4200/
